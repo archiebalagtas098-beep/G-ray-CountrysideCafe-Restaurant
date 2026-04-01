63a2a17620e40b6e983c9b9d103e1e6d59c27705
ChesterBaltazar <chesterbaltazar13@gmail.com>
2026-04-01 19:40:10 +0800

    first commit

---
+++ models/categoryModel.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const categorySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    index: true
+  },
+  isActive: {
+    type: Boolean,
+    default: true
+  },
+  image: String,
+  displayOrder: {
+    type: Number,
+    default: 0
+  }
+}, { 
+  timestamps: true,
+  collection: 'categories'
+});
+
+export default mongoose.models.Category || mongoose.model('Category', categorySchema);
